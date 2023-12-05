6d4389a8cb944686f3ac23d65dd18e1bf18e806d
RyTek <rytek1128@outlook.com>
2023-12-06 00:12:58 +0700

    Update * Make items removable * Add the amount to existing item instead of separate them

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -10,9 +10,13 @@ function Cart(props) {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  function cartItemRemoveHandler(id) {}
+  function cartItemRemoveHandler(id) {
+    cartCtx.removeItem(id);
+  }
 
-  function cartItemAddHandler(item) {}
+  function cartItemAddHandler(item) {
+    cartCtx.addItem({ ...item, amount: 1 });
+  }
 
   const cartitems = (
     <ul className={classes["cart-items"]}>
